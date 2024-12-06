b1a3eb4515421fda6f838747ae710a7fb2de9794
ushasreeus <ushasree037@gmail.com>
2024-12-06 19:40:33 +0530

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+// Script to handle button clicks
+document.getElementById("seeMore").addEventListener("click", function () {
+    alert("See More button clicked!");
+});
+
+document.getElementById("callToAction").addEventListener("click", function () {
+    alert("Call to Action button clicked!");
+});
